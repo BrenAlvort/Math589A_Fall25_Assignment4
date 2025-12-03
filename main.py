4c847e14b5f9972ccc6b4eb4ca87b7d7c0ad2e3e
BrenAlvort <bkao21@ciencias.unam.mx>
2025-12-03 12:22:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import fp_wrapper as fp
 
 def main():
-    x0 = 0.5
-    y0 = 0.5
-    tol = 1e-6
-    max_iter = 1000
-
     for k in range(10, 100, 10):
         scale = 1.0 / k
-        sol1, sol2, iters = fp.solve_system(x0, y0, tol, max_iter, scale)
-        print(f"scale = {scale:.3f} → iterations = {iters}")
+        x1, x2, iters = fp.solve_system(0.5, 0.5, 1e-6, 1000, scale)
+        print(f"scale={scale:0.3f} → iterations={iters}")
 
 if __name__ == "__main__":
     main()
+
